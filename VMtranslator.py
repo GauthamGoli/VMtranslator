596eb3cc3a6f1478024545799f14e715362fc3e6
Gautham Goli <goligautham@gmail.com>
2017-09-03 16:06:01 +0530

    Add main entry script

---
+++ VMtranslator.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from codewriter.codewriter import CodeWriter
+from parser.parser import Parser
+
+if(len(sys.argv)<2):
+	print "Supply file location."
+	sys.exit()
+else:
+	input_path = os.path.abspath(sys.argv[1])
+	parser = Parser(input_path)
+	output_path = input_path.replace('.vm', '.asm')
+	writer = CodeWriter(output_path)
+	while(parser.hasMoreCommands()):
+		parser.advance()
+		writer.writeCommand(parser.commandType, parser.arg1, parser.arg2)
+	writer.close()
+	print "Translation finished"
