7fa482c43da0b431ba86cc70c2cbeba6c031ff6c
Gautham Goli <goligautham@gmail.com>
2017-09-04 19:28:38 +0530

    Fix stack arithmetic command sequence bugs

--- codewriter/codewriter.py
+++ codewriter/codewriter.py
@@ -53,6 +53,7 @@ class CodeWriter:
             self.writeLines(command_seq)
 
     def writeArithmetic(self, command):
+        unique_label = self.unique_seq_key
         if command == 'add':
             command_seq = ['@SP','M=M-1','A=M','D=M','@SP','M=M-1','A=M','M=D+M', '@SP', 'M=M+1']
         elif command == 'sub':
@@ -60,7 +61,7 @@ class CodeWriter:
         elif command == 'neg':
             command_seq = ['@SP','A=M-1','M=-M']
         elif command == 'eq':
-            command_seq = ['@SP','M=M-1','A=M', 'D=M', '@SP', 'M=M-1', 'A=M', 'D=D-M','M=!D','D=0', 'D=!D', 'M=D&M', '@SP', 'M=M+1']
+            command_seq = ['@SP','M=M-1','A=M', 'D=M', '@SP', 'M=M-1', 'A=M','D=D-M', '@eq{}eq'.format(unique_label), 'D;JEQ', '@SP','A=M','M=0', '@neq{}neq'.format(unique_label), '0;JMP','(eq{}eq)'.format(unique_label),'@SP', 'A=M', 'M=-1','(neq{}neq)'.format(unique_label), '@SP', 'M=M+1']
         elif command == 'and':
             command_seq = ['@SP','M=M-1','A=M','D=M','@SP','M=M-1','A=M','D=D&M','M=D', '@SP','M=M+1']
         elif command == 'or':
@@ -68,11 +69,9 @@ class CodeWriter:
         elif command == 'not':
             command_seq = ['@SP','M=M-1','A=M','M=!M','@SP','M=M+1']
         elif command == 'lt':
-            unique_label = self.unique_seq_key
-            command_seq = ['@SP', 'M=M-1','A=M','D=M','@SP','M=M-1','A=M','@lt{}lt'.format(unique_label),'D=D-M;JLT','@nlt{}nlt'.format(unique_label),'0;JMP','(lt{}lt)'.format(unique_label),'@SP','A=M','D=0','M=!D','(nlt{}nlt)'.format(unique_label),'@SP','A=M','D=1','M=!D']
+            command_seq = ['@SP', 'M=M-1','A=M','D=M','@SP','M=M-1','A=M','D=M-D', '@lt{}lt'.format(unique_label),'D;JLT','@SP', 'A=M', 'M=0', '@nlt{}nlt'.format(unique_label),'0;JMP','(lt{}lt)'.format(unique_label),'@SP','A=M','M=-1', '(nlt{}nlt)'.format(unique_label),'@SP','M=M+1']
         elif command == 'gt':
-            unique_label = self.unique_seq_key
-            command_seq = ['@SP', 'M=M-1','A=M','D=M','@SP','M=M-1','A=M','@gt{}gt'.format(unique_label),'D=D-M;JGT','@ngt{}ngt'.format(unique_label),'0;JMP','(gt{}gt)'.format(unique_label),'@SP','A=M','D=0','M=!D','(ngt{}ngt)'.format(unique_label),'@SP','A=M','D=1','M=!D']
+            command_seq = ['@SP', 'M=M-1','A=M','D=M','@SP','M=M-1','A=M','D=M-D', '@gt{}gt'.format(unique_label),'D;JGT','@SP', 'A=M', 'M=0', '@ngt{}ngt'.format(unique_label),'0;JMP','(gt{}gt)'.format(unique_label),'@SP','A=M','M=-1', '(ngt{}ngt)'.format(unique_label),'@SP','M=M+1']
 
         command_seq.append('// {}\n'.format(command))
         self.writeLines(command_seq)
